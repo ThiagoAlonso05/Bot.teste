9d6669b0841c6b5c08092691aa3dcb128ae2beb6
ThiagoAlonso05 <thcsa2005@gmail.com>
2024-02-29 20:48:47 -0300

    Arrumando o gitignore

--- sql/script.sql
+++ sql/script.sql
@@ -1,12 +1,42 @@
+DROP schema if exists Calendario;
 CREATE DATABASE Calendario;
 USE Calendario;
 
+Drop table reserva;
 CREATE TABLE Reserva (
     idReserva INT NOT NULL AUTO_INCREMENT,
-    horarioReserva INT(4) NOT NULL,
-    Nome VARCHAR(50) NOT NULL,
-    CPF INT(11) NOT NULL,
-    Telefone INT(10) NOT NULL,
+    horarioReserva VARCHAR(50) NOT NULL,
+    Nome VARCHAR(500) NOT NULL,
+    CPF VARCHAR(150) NOT NULL,
+    Telefone VARCHAR(150) NOT NULL,
 
     PRIMARY KEY (idReserva)
 );
+
+SELECT * FROM Reserva;
+
+CREATE TABLE HorariosDisponiveis (
+    idHorario INT AUTO_INCREMENT,
+    horario TIME NOT NULL ,
+    disponivel BOOLEAN NOT NULL DEFAULT true,
+    PRIMARY KEY (idHorario)
+);
+
+Select * from HorariosDisponiveis;
+
+drop table HorariosDisponiveis;
+
+ALTER TABLE HorariosDisponiveis
+ADD UNIQUE (horario);
+
+
+INSERT INTO HorariosDisponiveis (horario, disponivel) VALUES
+('09:00', true),
+('10:00', true),
+('11:00', true),
+('12:00', true),
+('13:00', true),
+('14:00', true),
+('15:00', true),
+('16:00', true),
+('17:00', true);
